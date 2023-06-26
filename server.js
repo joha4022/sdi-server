3b580a16c9c0b1b261b98cc51f97388e2feb2281
joha4022 <joha4022@gmail.com>
2023-06-27 01:10:05 +0900

    condensed createServer

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const http = require('http');
 //   response.end();
 // });
 
-const server = http.createServer((request, response) => {
-  if (request.url.split('/')[1] === '') {
-    response.end("Hello Galvanize!");
-  }
-  response.statusCode = 404;
-  response.end();
-});
+// server.listen(3000, "localhost", () => {
+//   console.log(`Server running at http://localhost:3000/`);
+// });
 
+const server = http.createServer((req, res) => {
+  if(req.url.split('/')[1]==='') {
+    res.end('Hello Galvanize!');
+  }
+});
 
 server.listen(3000, "localhost", () => {
-  console.log(`Server running at http://localhost:3000/`);
+  console.log(`Server running on http://localhost:3000/`);
 });
-
 
